ab0f1a49960fe71b41eadd86fabc9c30e4bcff22
RubenEs <gsr_aem@hotmail.es>
2017-02-06 16:26:39 +0100

    Añadido Gato Rama 1

---
+++ src/Gato.java
@@ -0,0 +1,15 @@
+
+public class Gato {
+
+	int numeroVidas;
+
+	public int getNumeroVidas() {
+		return numeroVidas;
+	}
+
+	public void setNumeroVidas(int numeroVidas) {
+		this.numeroVidas = numeroVidas;
+	}
+
+	
+}
